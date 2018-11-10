ad2a25a752baed8c451c1c453fd54d2ffb17c959
DAVID-PC\David <oprutadavid@gmail.com>
2018-11-10 17:19:07 +0200

    added C version

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String args[])
-    {
-        int n = 50;
-        System.out.print("Following are the prime numbers ");
-        System.out.println("smaller than or equal to " + n);
-        SieveOfEratosthenes g = new SieveOfEratosthenes();
-        g.sieveOfEratosthenes(n);
+    public static void main(String args[]) {
+        int n;
+        System.out.println("Give n:");
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            try {
+                n = sc.nextInt();
+                System.out.print("Following are the prime numbers ");
+                System.out.println("smaller than or equal to " + n);
+                SieveOfEratosthenes g = new SieveOfEratosthenes();
+                g.sieveOfEratosthenes(n);
+            } catch (Exception e) {
+                System.out.println("Invalid number");
+            }
+        }
     }
 }
